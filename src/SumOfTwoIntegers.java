420629d52aaad2a4aed4a5a4845883a0044c9e77
Nathan Lui <nathanlui2010@gmail.com>
2018-10-05 14:32:35 -0700

    Added project stuff and travis stuff

---
+++ src/SumOfTwoIntegers.java
@@ -0,0 +1,13 @@
+
+public class SumOfTwoIntegers {
+
+	public static int sumOfIntegers(int i, int j)
+	{
+		return i + j;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(sumOfIntegers(1, 2));
+	}
+
+}
